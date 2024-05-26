e55a70675f96d634e71a8cd6d316e90072deb7fb
Lucas Ramos <lucas.ramos_dev@outlook.com>
2024-05-26 07:33:39 -0300

    função de deletar

--- py - web/views.py
+++ py - web/views.py
@@ -2,7 +2,6 @@ from flask import render_template, request, redirect, session, flash, url_for
 from index import app, db
 from models import Jogos, Usuarios
 
-lista_jogos = []
 
 @app.route('/')
 def index():
@@ -15,11 +14,12 @@ def novo():
         return redirect(url_for('login', proxima=url_for('novo')))
     return render_template('novo.html', titulo='Novo')
 
-@app.route('/editar')
-def editar():
+@app.route('/editar/<int:id>')
+def editar(id):
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect(url_for('login', proxima=url_for('editar')))
-    return render_template('editar.html', titulo='Editando')
+        return redirect(url_for('login', proxima=url_for('editar', id=id)))
+    jogo = Jogos.query.filter_by(id=id).first()
+    return render_template('editar.html', titulo='Editando', jogo=jogo)
 
 
 @app.route('/criar', methods=['POST',])
@@ -27,13 +27,46 @@ def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
-    jogo = Jogos(nome, categoria, console)
-    lista_jogos.append(jogo)
+
+    jogo = Jogos.query.filter_by(nome=nome).first()
+
+    if jogo:
+        flash('Jogo já existente')
+        return redirect(url_for('index'))
+
+    novo_jogo = Jogos(nome=nome, categoria=categoria, console=console)
+    db.session.add(novo_jogo)
+    db.session.commit()
+
     return redirect(url_for('index'))
 
 @app.route('/atualizar', methods=['POST',])
 def atualizar():
-    pass
+    print(request.form)
+    jogo = Jogos.query.filter_by(id=request.form['id']).first()
+    jogo.nome = request.form['nome']
+    jogo.categoria = request.form['categoria']
+    jogo.console = request.form['console']
+
+    db.session.add(jogo)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+
+@app.route('/deletar/<int:id>', methods=['GET'])
+def deletar(id):
+    print('daf')
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login'))
+    print('daf')
+    Jogos.query.filter_by(id=id).delete()
+    print('daf')
+    db.session.commit()
+    print('daf')
+    flash('Jogo Deletado com Sucesso!')
+
+    return redirect(url_for('index'))
 
 
 @app.route('/login')
